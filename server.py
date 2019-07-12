4b4cadf00c531e0d587c6c2b2e13fefabd41c3cd
Homuru <quangdt2510@gmail.com>
2019-07-12 17:19:40 +0700

    Upload file, create logs, sending message

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 import config
-from utils.message_handler import MessageHandler
+from utils.message_handler import Message_Handler
+from utils.datastore_handler import DataStore_Handler
+from utils.database_handler import Database_Handler
 from flask import Flask, request, flash, redirect
 from flask_cors import CORS, cross_origin
 from werkzeug.datastructures import ImmutableMultiDict
 import os
+import json
 import sys
 sys.path.append(os.getcwd())
-
+if not os.path.exists('tmp'):
+    os.makedirs('tmp')
 app = Flask(__name__)
+
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 CORS(app)
@@ -21,12 +26,22 @@ def hello():
 @app.route('/upload', methods=["POST"])
 def upload():
     # print(request)
-    # print(request.form)
+    print(request.form)
+    print("___")
     print(request.files.to_dict())
     f = request.files.to_dict()['data']
-    f.save('test.txt')
-    # TO DO
-    # Save the file to Minio and send file's URL 
+    filename = f.filename
+    from_path = 'tmp/'+filename
+    to_path = '/' + filename.split('.')[0] + '/raw/' + filename
+    f.save(from_path)
+    msg = {
+        "name": filename,
+        "type": filename.split('.')[1],
+        "date": request.form['date'],
+        "file_uri": to_path
+    }
+    Message_Handler.sendMessage('from_client', json.dumps(msg))
+    Database_Handler.insert(msg)
     return 'Hello'
 
 
@@ -36,5 +51,4 @@ def getModel(model_id):
 
 
 if __name__ == '__main__':
-    MessageHdlr = MessageHandler(config.RABBITMQ_CONNECTION)
     app.run(debug=True, host='0.0.0.0', port='8000')
